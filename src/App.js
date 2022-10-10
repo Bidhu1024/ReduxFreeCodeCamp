0a08f34fa1fd791992ffa22915a7e35b5a918927
BidhubhushanGahan <bidhu1024av@gmail.com>
2022-10-10 18:15:27 +0530

    Counter App Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { useSelector } from 'react-redux';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
 
 function App() {
-  const counter = useSelector((state)=>state.counter)
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({type:'DEC'})
+  };
+ 
   return (
-    <div >
+    <div>
       <h1>Counter App</h1>
-      <h2>{counter}</h2>
+      <h2>{counter} </h2>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,7 +1,16 @@
 import {createStore} from 'redux';
 
 const reducerFn = (state={counter:0},action)=>{
-return state 
+
+if(action.type==='INC'){
+    return {counter: state.counter+1}
+}
+if(action.type==="DEC"){
+    return {counter: state.counter - 1}
+}
+    return state
+
+
 }
 const store = createStore(reducerFn);
 export default store;
